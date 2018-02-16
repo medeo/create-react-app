99f819760f75d7877d7e84aeb1e309052a81d6b8
Charles-Antoine Sohier <charles@medeo-health.com>
2018-02-16 10:41:45 +0100

    refactor(App): replace by a functional component

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -1,18 +1,9 @@
 import React, { Component } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = ({className} ) => (
+  <div className={className}>
+    <p>Hello <strong>Medeo</strong></p>
+  </div>
+)
 
 export default App;
